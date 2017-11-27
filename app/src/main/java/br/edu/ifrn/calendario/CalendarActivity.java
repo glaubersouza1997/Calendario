d513c3b1b691fbc0c3cd3e7dde474ebcc4cb651c
201520111160034 <teste@teste.org>
2017-11-27 17:41:37 -0300

    commit

--- app/src/main/java/br/edu/ifrn/calendario/CalendarActivity.java
+++ app/src/main/java/br/edu/ifrn/calendario/CalendarActivity.java
@@ -2,7 +2,6 @@ package br.edu.ifrn.calendario;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -17,9 +16,10 @@ public class CalendarActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_layout);
         mCalendarView = (CalendarView)findViewById(R.id.calendarView);
+
         mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
                 String date = (i1 + 1)+ "/" + i2 + "/" + i;
                 Log.d(TAG, "onSelectedDayChange: mm/dd/yyyy:" + date);
                 Intent intent = new Intent(CalendarActivity.this , MainActivity.class);
